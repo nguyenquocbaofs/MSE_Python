31e27f55d0b41cd0018ab1742b0118cf220e3f42
Nhon Le <nhonlemobileapp@gmail.com>
2025-01-13 09:55:17 +0700

    update home page and detail

--- product_view_app/lib/presentation/views/detail_page_view.dart
+++ product_view_app/lib/presentation/views/detail_page_view.dart
@@ -10,13 +10,145 @@ class DetailPageView extends StatefulWidget {
 class _DetailPageViewState extends State<DetailPageView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    TextEditingController controller = TextEditingController();
+    int dummyCurrentId = 1;
+    String dummyComment = "This is my comment to test";
+    String dummyUrl =
+        "https://fastly.picsum.photos/id/9/250/250.jpg?hmac=tqDH5wEWHDN76mBIWEPzg1in6egMl49qZeguSaH9_VI";
+
+    Widget commonSpaceColumn(
+        {required List<Widget> children,
+        double space = 5,
+        CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+        MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start}) {
+      List<Widget> spacedChildren = [];
+
+      for (int i = 0; i < children.length; i++) {
+        spacedChildren.add(children[i]);
+
+        if (i != children.length - 1) {
+          spacedChildren.add(
+            SizedBox(height: space),
+          );
+        }
+      }
+
+      return Column(
+        crossAxisAlignment: crossAxisAlignment,
+        mainAxisAlignment: mainAxisAlignment,
+        children: spacedChildren,
+      );
+    }
+
+    buildListComments() {
+      List<Widget> listComments = <Widget>[];
+      Widget buildItems(int id) {
+        return Row(
+          children: [
+            Text(
+              "Name: ---- ",
+              style: TextStyle(
+                  color: id != dummyCurrentId ? Colors.black : Colors.red),
+            ),
+            const SizedBox(
+              width: 12,
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(16.0),
+                ),
+              ),
+              child: Text(dummyComment),
+            ),
+          ],
+        );
+      }
+
+      for (var i = 0; i < 20; i++) {
+        listComments.add(buildItems(i));
+      }
+      return listComments;
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.green.shade50,
+      appBar: AppBar(
+        title: const Text(
+          "Product Detail",
+        ),
+        backgroundColor: Colors.blue,
+      ),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Center(
-            child: Text("Detail"),
-          ),
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: commonSpaceColumn(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    space: 12.0,
+                    children: <Widget>[
+                      Card(
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: commonSpaceColumn(
+                            children: <Widget>[
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Image.network(dummyUrl),
+                              const Text("Product Title:"),
+                              const Text("Product vote: "),
+                              const Text("Score: "),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const Text("How do you feel this product?"),
+                      Container(
+                        padding: const EdgeInsets.all(12.0),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(16.0),
+                          ),
+                        ),
+                        child: TextField(
+                          textCapitalization: TextCapitalization.sentences,
+                          controller: controller,
+                          keyboardType: TextInputType.multiline,
+                          textInputAction: TextInputAction.newline,
+                          minLines: 1,
+                          maxLines: 8,
+                          decoration: const InputDecoration(
+                            hintText: 'Write your comments',
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: ElevatedButton(
+                          onPressed: () => {},
+                          child: const Text("Comments"),
+                        ),
+                      ),
+                      commonSpaceColumn(
+                        space: 12.0,
+                        children: buildListComments(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
--- product_view_app/lib/presentation/views/home_page_view.dart
+++ product_view_app/lib/presentation/views/home_page_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:product_view_app/presentation/views/detail_page_view.dart';
+
 class HomePageView extends StatefulWidget {
   const HomePageView({super.key});
 
@@ -10,13 +12,172 @@ class HomePageView extends StatefulWidget {
 class _HomePageViewState extends State<HomePageView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Center(
-            child: Text("Home"),
+    String dummyUrl =
+        "https://fastly.picsum.photos/id/9/250/250.jpg?hmac=tqDH5wEWHDN76mBIWEPzg1in6egMl49qZeguSaH9_VI";
+    Widget buildBestProductList() {
+      buildItem(int index) {
+        subText(String txtContent) {
+          return Text(
+            '$txtContent $index',
+            softWrap: true,
+            style: const TextStyle(fontSize: 12.0),
+          );
+        }
+
+        return Container(
+            width: 220,
+            margin: const EdgeInsets.all(5.0),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(16.0),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.topCenter,
+                    height: 80.0,
+                    margin: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+                    child: Image.network(dummyUrl),
+                  ),
+                  subText("Product name"),
+                  subText("Score: 1000"),
+                  subText("Comments: 1700"),
+                ],
+              ),
+            ));
+      }
+
+      List<Widget> listItem() {
+        final items = <Widget>[];
+
+        for (var i = 0; i < 20; i++) {
+          items.add(buildItem(i));
+        }
+        return items;
+      }
+
+      return Container(
+        color: Colors.yellow.shade100,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.volunteer_activism_rounded),
+                    Text("Best Products"),
+                  ],
+                )),
+            SizedBox(
+              height: 160.0,
+              width: MediaQuery.of(context).size.width,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: listItem(),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    buildContentProductList() {
+      buildItem(int index) {
+        subText(String txtContent) {
+          return Text(
+            '$txtContent $index',
+            softWrap: true,
+            style: const TextStyle(fontSize: 12.0),
+          );
+        }
+
+        return InkWell(
+          onTap: () => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const DetailPageView(),
+              ),
+            )
+          },
+          child: Container(
+            height: 120,
+            margin: const EdgeInsets.all(5.0),
+            decoration: const BoxDecoration(
+              color: Colors.white54,
+              borderRadius: BorderRadius.all(
+                Radius.circular(16.0),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: Image.network(dummyUrl),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        subText("Product name"),
+                        subText("Score"),
+                        subText("Comments"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
+        );
+      }
+
+      List<Widget> listItem() {
+        final items = <Widget>[];
+
+        for (var i = 0; i < 20; i++) {
+          items.add(buildItem(i));
+        }
+        return items;
+      }
+
+      return ListView(
+        scrollDirection: Axis.vertical,
+        children: listItem(),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.green.shade50,
+      appBar: AppBar(
+        title: const Text("Product Views"),
+        backgroundColor: Colors.blue,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            buildBestProductList(),
+            const Padding(
+              padding: EdgeInsets.only(top: 12.0, bottom: 12.0),
+              child: Text("List Products"),
+            ),
+            Expanded(
+              child: buildContentProductList(),
+            ),
+          ],
         ),
       ),
     );
