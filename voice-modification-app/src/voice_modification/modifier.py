c1bb5b3e01bda1c1c697346ea7f4ac0dbac97f27
Bao Nguyen Quoc <baonguyenquoc357@gmail.com>
2025-03-27 21:28:05 +0700

    commit latest

--- voice-modification-app/src/voice_modification/modifier.py
+++ voice-modification-app/src/voice_modification/modifier.py
@@ -19,10 +19,9 @@ class VoiceModifier:
         if not os.path.isfile(input_audio_path):
             print(f"Error: File '{input_audio_path}' does not exist.")
             return
-        audio, sr = librosa.load(input_audio_path, sr=None)
+        audio, sr = librosa.load(input_audio_path, sr=16000)
 
-        # gender = self.detect_gender(audio, sr)
-        # print("Gender: ", gender)
+        self.detect_gender(audio, sr, filename="f0.png")
 
         # Remove all existing images in the directory
         os.makedirs(IMAGE_DIR, exist_ok=True)
@@ -31,15 +30,17 @@ class VoiceModifier:
             if os.path.isfile(file_path):
                 os.remove(file_path)
 
-        # Draw the original audio waveform and spectrogram
+        # Draw the original audio waveform and spectrogram and chroma
         self.draw_waveform(audio, sr, title="Original Audio Waveform", filename="original_waveform.png")
         self.draw_spectrogram(audio, sr, title="Original Audio Spectrogram", filename="original_spectrogram.png")
+        # self.draw_chroma(audio, sr, title="Original Audio Chroma", filename="original_spectrogram.png")
 
         # Apply pitch shift
         if 'pitch_shift' in modification_parameters and modification_parameters['pitch_shift'] is not None:
             audio = librosa.effects.pitch_shift(audio, sr=sr, n_steps=modification_parameters['pitch_shift'])
             self.draw_waveform(audio, sr, title="Pitch Shifted Audio Waveform", filename="pitch_shifted_waveform.png")
             self.draw_spectrogram(audio, sr, title="Pitch Shifted Audio Spectrogram", filename="pitch_shifted_spectrogram.png")
+            #self.draw_chroma(audio, sr, title="Pitch Shifted Audio Chroma", filename="pitch_shifted_spectrogram.png")
 
         # Apply speed change
         if 'speed_change' in modification_parameters and modification_parameters['speed_change'] is not None:
@@ -82,13 +83,36 @@ class VoiceModifier:
         plt.savefig(filepath)
         plt.close()
     
-    def plot_spectrogram(self, y, sr, title="Spectrogram", filename="spectrogram.png"):
+    def draw_chroma(self, audio, sr, title="Chroma", filename="chroma.png"):
         filepath = os.path.join(IMAGE_DIR, filename)
 
-        D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+        chroma = librosa.feature.chroma_stft(y=audio, sr=sr)
         plt.figure(figsize=(10, 4))
-        librosa.display.specshow(D, sr=sr, x_axis="time", y_axis="log")
-        plt.colorbar(format="%+2.0f dB")
+        librosa.display.specshow(chroma, sr=sr, x_axis="time", y_axis="chroma", cmap='coolwarm')
+        plt.colorbar()
         plt.title(title)
         plt.savefig(filepath)
         plt.close()
+
+    def detect_gender(self, audio, sr, filename="f0.png"):
+        # calculate magnitude 
+        N = len(audio)
+        Y = np.fft.fft(audio)
+        frequencies = np.fft.fftfreq(N, d=1/sr)
+
+        # Chỉ lấy phần phổ tần số dương
+        positive_freqs = frequencies[:N//2]
+        magnitude = np.abs(Y[:N//2])
+
+        # get max magnitude
+        f0_idx = np.argmax(magnitude[1:]) + 1
+        f0 = positive_freqs[f0_idx]
+
+        print(f"F0: {f0:.2f} Hz")
+
+        if 85 <= f0 <= 180:
+            print("Gender: Male ")
+        elif 165 <= f0 <= 255:
+            print("Gender: Female")
+        else:
+            print("Unknown")
